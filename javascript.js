1a6c8b4035e84782ff85630b69ce0f2138d109c5
Jayden Traynor <celador24510@gmail.com>
2024-01-26 10:35:40 +0800

    Finalize game.
    HTML and CSS updated.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,53 @@
+/********************
+ SCISSORS PAPER ROCK
+*********************/
 
+// GLOBAL VARIABLES:
+let computerChoice;
+let playerChoice;
+let result;
+let resolved;
+let message = "Choose scissors, paper or rock!";
+let textbox = document.querySelector("#textbox");
+let button = document.querySelector("#playgame");
+
+button.addEventListener("click", runGame);
+
+// Main game loop. Runs game, alerts player of result, and handles draws and incorrect input.
+async function runGame(){
+
+    resolved = false;
+    
+    while (!resolved) {
+        // Pauses loop to allow textbox to render update
+        await sleep(0);
+        try {
+            computerChoice = getComputerChoice();
+            playerChoice = getPlayerChoice(message);
+            if (playerChoice === 'cancel') {
+                break;
+            }
+            result = calculateResult(playerChoice, computerChoice);
+        
+            if(result == 'win'){
+                textbox.textContent += `Congratulations, you win! ${playerChoice} beats ${computerChoice}\n`;
+                resolved = true;
+            } else if (result == 'loss') {
+                textbox.textContent += `Sorry, you lose... ${playerChoice} loses to ${computerChoice}\n`;
+                resolved = true;
+            } else {
+                textbox.textContent += (message = `It was a draw! ${playerChoice} vs ${computerChoice}. Try again!`) + "\n";
+            }
+        } catch (e) {
+            
+            console.error(`${e.message}\n`);
+            message = "Invalid selection. Try again."
+            
+        }
+    }
+}
+
+// Calculates and returns computer choice
 function getComputerChoice(){    
     switch (Math.floor(Math.random() * 3) + 1) {
         case 1:
@@ -10,14 +59,21 @@ function getComputerChoice(){
     }
 }
 
+// Returns player choice from input prompt
 function getPlayerChoice(message){
-    let choice = prompt(message).toLowerCase();
+    let choice = prompt(message);
+    // Stop game if prompt cancelled
+    if(choice === null) {
+        return 'cancel';
+    }
+    choice = choice.toLowerCase();
     if((choice !== 'scissors') && (choice !== 'paper') && (choice !== 'rock')) {
         throw new Error("Invalid selection. Please choose 'scissors', 'paper' or 'rock'.");            
     }
     return choice;
 }
 
+// Takes both player and computer choices and returns 'win', 'loss or 'draw'
 function calculateResult(playerChoice, computerChoice){
 
     switch (playerChoice) {
@@ -37,30 +93,8 @@ function calculateResult(playerChoice, computerChoice){
     return 'loss';
 }
 
-let computerChoice;
-let playerChoice;
-let result;
-let resolved = false;
-let message = "Choose scissors, paper or rock!";
-
-while (!resolved) {
-
-    try {
-        computerChoice = getComputerChoice();
-        playerChoice = getPlayerChoice(message);
-        result = calculateResult(playerChoice, computerChoice);
-    
-        if(result == 'win'){
-            console.log(`Congratulations, you win! ${playerChoice} beats ${computerChoice}`);
-            resolved = true;
-        } else if (result == 'loss') {
-            console.log(`Sorry, you lose... ${playerChoice} loses to ${computerChoice}`);
-            resolved = true;
-        } else {
-            message = `It was a draw! ${playerChoice} vs ${computerChoice}. Try again!`;
-        }
-    } catch (e) {
-        console.error(e.message);
-        message = "Invalid selection. Try again."
-    }
+// Helper function for async
+function sleep(ms) {
+    return new Promise(resolve => 
+        setTimeout(resolve, ms));
 }
