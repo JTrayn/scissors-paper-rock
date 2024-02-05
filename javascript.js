b3beea76c9bcbdf60a492ba5867f26fc15281498
Jayden Traynor <celador24510@gmail.com>
2024-02-05 14:10:34 +0800

    Update GUI
    GUI updated. Input replaced with buttons.

--- javascript.js
+++ javascript.js
@@ -2,52 +2,75 @@
  SCISSORS PAPER ROCK
 *********************/
 
-// GLOBAL VARIABLES:
 let computerChoice;
 let playerChoice;
-let result;
-let resolved;
-let message = "Choose scissors, paper or rock!";
-let textbox = document.querySelector("#textbox");
-let button = document.querySelector("#playgame");
+let scissorButton = document.querySelector('.scissors');
+let paperButton = document.querySelector('.paper');
+let rockButton = document.querySelector('.rock');
+let textBox = document.querySelector('.result');
+let playerScore = document.querySelector('.card-score-player');
+let computerScore = document.querySelector('.card-score-computer');
+let card = document.querySelector('.card');
 
-button.addEventListener("click", runGame);
+//------------------------------------------------------------
 
-// Main game loop. Runs game, alerts player of result, and handles draws and incorrect input.
-async function runGame(){
+scissorButton.addEventListener('click', e => {
+    runGame('scissors');
+});
+paperButton.addEventListener('click', e => {
+    runGame('paper');
+});
+rockButton.addEventListener('click', e => {
+    runGame('rock');
+}); 
 
-    resolved = false;
-    
-    while (!resolved) {
-        // Pauses loop to allow textbox to render update
-        await sleep(0);
-        try {
-            computerChoice = getComputerChoice();
-            playerChoice = getPlayerChoice(message);
-            if (playerChoice === 'cancel') {
-                break;
-            }
-            result = calculateResult(playerChoice, computerChoice);
+
+/**
+ * @param {String} choice 
+ */
+function runGame(choice){
+
+    computerChoice = getComputerChoice();
+    playerChoice = choice;
+    textBox.textContent = calculateResult(playerChoice, computerChoice);
+
+    if(+playerScore.textContent === 5) {
+        textBox.textContent = `You won the match! First to 5!`;
+        playAgain();
         
-            if(result == 'win'){
-                textbox.textContent += `Congratulations, you win! ${playerChoice} beats ${computerChoice}\n`;
-                resolved = true;
-            } else if (result == 'loss') {
-                textbox.textContent += `Sorry, you lose... ${playerChoice} loses to ${computerChoice}\n`;
-                resolved = true;
-            } else {
-                textbox.textContent += (message = `It was a draw! ${playerChoice} vs ${computerChoice}. Try again!`) + "\n";
-            }
-        } catch (e) {
-            
-            console.error(`${e.message}\n`);
-            message = "Invalid selection. Try again."
-            
-        }
+    } else if(+computerScore.textContent === 5) {
+        textBox.textContent = `You lost the match! Computer won first to 5!`;
+        playAgain();
     }
 }
 
-// Calculates and returns computer choice
+function playAgain() {
+
+    scissorButton.disabled = true;
+    paperButton.disabled = true;
+    rockButton.disabled = true;
+
+    let playAgainButton = document.createElement('button');
+    playAgainButton.textContent = 'Play again?';
+    playAgainButton.classList.add('button');
+    playAgainButton.style.width = '150px';
+    playAgainButton.style.alignSelf = 'center';
+    card.appendChild(playAgainButton);
+
+    playAgainButton.addEventListener('click', e => {
+        playerScore.textContent = 0;
+        computerScore.textContent = 0;
+        textBox.textContent = 'New game. First to 5 wins!';
+        scissorButton.disabled = false;
+        paperButton.disabled = false;
+        rockButton.disabled = false;
+        playAgainButton.remove();
+    });
+}
+
+/**
+ * @returns 
+ */
 function getComputerChoice(){    
     switch (Math.floor(Math.random() * 3) + 1) {
         case 1:
@@ -59,40 +82,36 @@ function getComputerChoice(){
     }
 }
 
-// Returns player choice from input prompt
-function getPlayerChoice(message){
-    let choice = prompt(message);
-    // Stop game if prompt cancelled
-    if(choice === null) {
-        return 'cancel';
-    }
-    choice = choice.toLowerCase();
-    if((choice !== 'scissors') && (choice !== 'paper') && (choice !== 'rock')) {
-        throw new Error("Invalid selection. Please choose 'scissors', 'paper' or 'rock'.");            
-    }
-    return choice;
-}
-
-// Takes both player and computer choices and returns 'win', 'loss or 'draw'
+/**
+ * @param {String} playerChoice 
+ * @param {String} computerChoice 
+ * @returns 
+ */
 function calculateResult(playerChoice, computerChoice){
 
-    if (playerChoice === computerChoice) return 'draw';
+    if (playerChoice === computerChoice) {
+        return `It was a draw! ${playerChoice} vs ${computerChoice}`;
+    }
     switch (playerChoice) {
         case 'scissors':
-            if(computerChoice === 'paper') return 'win';
+            if(computerChoice === 'paper') {
+                playerScore.textContent = +playerScore.textContent + 1;
+                return `You won! ${playerChoice} beats ${computerChoice}`;
+            }
             break;
         case 'paper':
-            if(computerChoice === 'rock') return 'win';
+            if(computerChoice === 'rock') {
+                playerScore.textContent = +playerScore.textContent + 1;
+                return `You won! ${playerChoice} beats ${computerChoice}`;
+            }
             break;
         case 'rock':
-            if(computerChoice === 'scissors') return 'win';
+            if(computerChoice === 'scissors') {
+                playerScore.textContent = +playerScore.textContent + 1;
+                return `You won! ${playerChoice} beats ${computerChoice}`;
+            }
             break;
     }
-    return 'loss';
-}
-
-// Helper function for async
-function sleep(ms) {
-    return new Promise(resolve => 
-        setTimeout(resolve, ms));
+    computerScore.textContent = +computerScore.textContent + 1;
+    return `You lost! ${playerChoice} loses to ${computerChoice}`;
 }
