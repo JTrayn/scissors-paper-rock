cda02ccd86261110b320d5035def6894d67830f1
Jayden Traynor <celador24510@gmail.com>
2024-02-05 16:25:13 +0800

    Add percentage win chance tracker
    Added percentage win chance for player and computer.

--- javascript.js
+++ javascript.js
@@ -11,6 +11,8 @@ let textBox = document.querySelector('.result');
 let playerScore = document.querySelector('.card-score-player');
 let computerScore = document.querySelector('.card-score-computer');
 let card = document.querySelector('.card');
+let playerWinText = document.querySelector('.player-chance');
+let computerWinText = document.querySelector('.computer-chance');
 
 //------------------------------------------------------------
 
@@ -24,6 +26,51 @@ rockButton.addEventListener('click', e => {
     runGame('rock');
 }); 
 
+
+function updateChanceText() {
+
+    let playerWinChance = calculateWinPercentage(+playerScore.textContent, +computerScore.textContent);
+    let computerWinChance = 100 - playerWinChance;
+    if(playerWinChance > 50) {
+        playerWinText.style.color = 'lightgreen';
+        computerWinText.style.color = 'red';
+    } else if(playerWinChance < 50) {
+        playerWinText.style.color = 'red';
+        computerWinText.style.color = 'lightgreen';
+    } else {
+        playerWinText.style.color = 'blue';
+        computerWinText.style.color = 'blue';
+    }
+    playerWinText.textContent = `${playerWinChance}%`;
+    computerWinText.textContent = `${computerWinChance}%`;
+    
+
+    /**
+     * @param {Number} playerScore 
+     * @param {Number} computerScore 
+     * @returns 
+     */
+    function calculateWinPercentage(playerScore, computerScore) {
+
+        let CHANCE = 0.5;
+        let ROUNDS = 5;
+        let playerChance = CHANCE ** (ROUNDS - playerScore);
+        let computerChance = CHANCE ** (ROUNDS - computerScore);
+        let factor = computerChance / playerChance;
+
+        if (playerScore === 5) {
+            return 100;
+        } else if (computerScore === 5) {
+            return 0;
+        } else if (playerScore === computerScore) {
+            return 0.5 * 100;
+        } else if (playerScore > computerScore) {
+            return (1 - (factor * CHANCE)) * 100;
+        } else if (computerScore > playerScore) {
+            return CHANCE / factor * 100;
+        }
+    }
+}
 
 /**
  * @param {String} choice 
@@ -61,6 +108,7 @@ function playAgain() {
         playerScore.textContent = 0;
         computerScore.textContent = 0;
         textBox.textContent = 'New game. First to 5 wins!';
+        updateChanceText();
         scissorButton.disabled = false;
         paperButton.disabled = false;
         rockButton.disabled = false;
@@ -91,27 +139,32 @@ function calculateResult(playerChoice, computerChoice){
 
     if (playerChoice === computerChoice) {
         return `It was a draw! ${playerChoice} vs ${computerChoice}`;
+        
     }
     switch (playerChoice) {
         case 'scissors':
             if(computerChoice === 'paper') {
                 playerScore.textContent = +playerScore.textContent + 1;
+                updateChanceText();
                 return `You won! ${playerChoice} beats ${computerChoice}`;
             }
             break;
         case 'paper':
             if(computerChoice === 'rock') {
                 playerScore.textContent = +playerScore.textContent + 1;
+                updateChanceText();
                 return `You won! ${playerChoice} beats ${computerChoice}`;
             }
             break;
         case 'rock':
             if(computerChoice === 'scissors') {
                 playerScore.textContent = +playerScore.textContent + 1;
+                updateChanceText();
                 return `You won! ${playerChoice} beats ${computerChoice}`;
             }
             break;
     }
     computerScore.textContent = +computerScore.textContent + 1;
+    updateChanceText();
     return `You lost! ${playerChoice} loses to ${computerChoice}`;
 }
